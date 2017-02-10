6c1c4bc7bd6be45e0666c017b1de84b0a6682148
eileenyangg <eileenyang.ey@gmail.com>
2017-02-09 22:10:24 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -100,6 +100,7 @@ $(function(){
        "images/home-bg-slideshow1.jpg", 
        "images/home-bg-slideshow2.jpg",
        "images/home-bg-slideshow3.jpg",
+       "images/home-bg-slideshow4.jpg"
         ],  {duration: 2000, fade: 750});
     });
   })
